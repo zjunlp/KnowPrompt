92f6f09fb885bf4a143164c9a7415b6220c7ad5d
njcx-ai <chenxiang_nj@outlook.com>
2022-01-14 09:53:27 +0800

    Create CITATION.c

---
+++ CITATION.c
@@ -0,0 +1,47 @@
+cff-version: "1.0.0"
+message: "If you use the code, please cite the following paper:"
+title: "deepke"
+repository-code: "https://https://github.com/zjunlp/DeepKE"
+authors: 
+  - family-names: Chen
+    given-names: Xiang
+  - family-names: Zhang
+    given-names: Ningyu
+  - family-names: Xie
+    given-names: Xin
+  - family-names: Deng
+    given-names: Shumin
+  - family-names: Yao
+    given-names: Yunzhi
+  - family-names: Tan
+    given-names: Chuanqi
+  - family-names: Huang
+    given-names: Fei
+  - family-names: Si
+    given-names: Luo
+  - family-names: Chen
+    given-names: Huajun
+preferred-citation:
+  type: article
+  title: "KnowPrompt: Knowledge-aware Prompt-tuning with Synergistic Optimization for Relation Extraction"
+  authors:
+  - family-names: Chen
+    given-names: Xiang
+  - family-names: Zhang
+    given-names: Ningyu
+  - family-names: Xie
+    given-names: Xin
+  - family-names: Deng
+    given-names: Shumin
+  - family-names: Yao
+    given-names: Yunzhi
+  - family-names: Tan
+    given-names: Chuanqi
+  - family-names: Huang
+    given-names: Fei
+  - family-names: Si
+    given-names: Luo
+  - family-names: Chen
+    given-names: Huajun
+  journal: "International World Wide Web Conferences (WWW)"
+  year: 2022
