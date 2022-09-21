b993f44362bb5e8dc13ebd91691f1efa094a388e
CheaSim <31753427+CheaSim@users.noreply.github.com>
2022-09-21 19:59:25 +0800

    Update semeval.sh

--- scripts/semeval.sh
+++ scripts/semeval.sh
@@ -1,6 +1,6 @@
 CUDA_VISIBLE_DEVICES=0 python main.py --max_epochs=10  --num_workers=8 \
     --model_name_or_path roberta-large \
-    --accumulate_grad_batches 1 \
+    --accumulate_grad_batches 4 \
     --batch_size 16 \
     --data_dir dataset/semeval \
     --check_val_every_n_epoch 1 \
